e781d5eeeb061d8db9778f154a388e57bc5c4450
Andrew Chang <acchang4@gmail.com>
2020-05-25 20:34:19 -0400

    completed exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
-}
-
+const repeatString = function(words, times) {
+    let phrase = '';
+    if (times < 0) {return 'ERROR'}
+    else {
+    for (let i = 0; i < times; i++) {
+    (phrase = phrase + words);}
+    return phrase;}
+    };
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
-}
-
+const reverseString = function(sentence) {
+    let newsentence = '';
+    for (i = sentence.length-1; i >= 0; i--) {
+        (newsentence += sentence[i]);}
+    return(newsentence)
+};
 module.exports = reverseString
