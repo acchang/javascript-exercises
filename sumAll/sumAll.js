03fb0dc974b05471d75c337624895cd67f4eadb3
Andrew Chang <acchang4@gmail.com>
2020-05-30 17:05:59 -0400

    finished exercise 5 - sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    if (x < 0 || y < 0 || typeof y != 'number' || typeof x != 'number') {return('ERROR')}
+    if (x > y) {
+	 big = x;
+	 small = y;}
+    else {big = y; 
+	 small = x;}
+    var array = [];
+    let i = small;
+    while (i <= big) { 
+      array.push( i );
+      i++;
+    }
+    return array.reduce(function(total, current) {return total + current});    
 }
 
 module.exports = sumAll
