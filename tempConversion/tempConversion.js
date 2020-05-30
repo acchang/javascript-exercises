ebe26c44c5ee2420231299cb9a57a2a2c18bc554
Andrew Chang <acchang4@gmail.com>
2020-05-30 19:52:03 -0400

    finished conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsiusAll = (temp - 32) * (5/9);
+  let celsiusOne = Math.round(celsiusAll * 10)/10;
+  return celsiusOne
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let FahAll = temp * (9/5) + 32;
+  let FahOne = Math.round(FahAll * 10)/10;
+  return FahOne
+  
 }
 
 module.exports = {
