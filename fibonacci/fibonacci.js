69a296c51049983f3647814c74ca8b29f03e1e3d
Andrew Chang <acchang4@gmail.com>
2020-06-21 22:37:43 -0400

    added line for fib(0)

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,6 +1,7 @@
 const fibonacci = function(x) {
     if (x == 1){return 1};
     if (x == 2){return 1};
+    if (x == 0){return 0};
     if (x < 0) {return "OOPS"};
     let y = x - 2;
     let array = [1, 1]
